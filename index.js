b49cd60465d29b549fd3232d89418595f0a5e2b8
Medrine Musabi Mulindi <medrine.mulindi@student.moringaschool.com>
2023-07-11 08:42:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 22;
+let num4 = 6
+let multiply=num1 * num2;
+const random = Math.floor(Math.random() * 100) + 1;
+let mod = num3 % num4;
+let max = Math.max(5,12,20);
